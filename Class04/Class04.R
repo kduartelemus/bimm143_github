5e08c93fc1e311d9a2c2cf395d67eb083c6290ea
Keilyn Duarte <kduartelemus@ucsd.edu>
2025-02-25 11:36:59 -0800

    Add some initial class project

---
+++ Class04/Class04.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Keilyn Duarte Lemus (PID A16881868)"
+#' date: "January 16, 2025"
+#' ---
+
+
+x<-1:50
+plot(x,sin(x))
+
+#Make this nicer
+plot(x,sin(x), col="pink", type="l", lwd=3)
+
